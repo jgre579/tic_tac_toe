3fa9e11047d060cc0951a1161b9e4aab363d22a3
jgre579 <jgre579@aucklanduni.ac.nz>
2022-06-29 17:43:38 +1200

    Add user move input

--- main.c
+++ main.c
@@ -12,16 +12,19 @@
 /* Function prototypes */
 void print_board(int board[3][3]);
 void print_title();
+void get_move(char symbol, int board[3][3]);
+
 
 int main(int argc, char const *argv[]) {
     /* code */
 
+    bool is_running = false;
     static int board[3][3] = {
         {X,X,X},
         {SPACE, SPACE, SPACE},
         {O,O,O}
-
     };
+    
     print_title();
     print_board(board);
     
@@ -34,11 +37,51 @@ int main(int argc, char const *argv[]) {
         scanf(" %c", &input);
     }
     while (input != 83);
+    is_running = true;
 
+    // Game Loop
+    while (is_running) {
 
+        // Get player 1 input
+        get_move(X, board);
+        print_board(board);
+        // Process move
+        
+        // Check win 
+
+        // Repeat for player 2. 
+        is_running = true;
+    }
 
     return 0;
 }
+
+
+
+void get_move(char symbol, int board[3][3]) {
+
+    int x, y;
+
+    printf("\nPlayer %c: Enter the coordinates of your next mode\n", symbol);
+
+    while (true) {
+        scanf(" %d %d", &x, &y);
+        if (x > 2 || x < 0 || y > 2 ||  x < 0) {
+            printf("\nInvalid coordinates, out of bounds. \n");
+        }
+        else if (board[x][y] != SPACE) {
+            printf("\nInvalid coordinates, space already occupied. \n");
+        }
+        else {
+            break;
+        }    
+    }
+
+    board[x][y] = symbol;
+
+}
+
+
 
 void print_title() {
     printf("------------------------------------------------\n");
